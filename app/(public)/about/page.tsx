6fa57fb6460b7c526fd5d171c705e8c18783bcef
Nam <hoangnamngx882@gmail.com>
2023-11-13 14:48:03 -0800

    Responsive layout for about page

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -3,45 +3,24 @@ import Link from 'next/link'
 
 export default function About() {
     return (
-        <div className="flex mx-60">
+        <div className="md:flex md:items-center mx-10 sm:mx-24 xl:mx-60  text-[#b3b3b3]">
+            <div className='mb-5 md:mr-12 lg:mr-28 '>
+                <p className='pb-5 font-semibold text-center sm:text-left text-2xl text-[#ce9c4b]'>ABOUT ME</p>
 
-            <div className='mr-28'>
-                <p className='pb-7 font-semibold text-2xl text-[#ce9c4b]'>ABOUT ME</p>
+                <p className='mb-5'>Hey, this is Nam. I adwadwam from Hanoi, Vietnam and currently living in Canada.</p>
 
-                <p className='mb-5 text-lg'>Hey, this is Nam. I am from Hanoi, Vietnam and currently living in Canada.</p>
-
-                <p className='text-lg'> I am a Web Developer with a Diploma in Computer Studies
-                    from Langara College in 2022.
+                <p className=''> I am a Web Developer with a Diploma in Computer Studies
+                    from Langara College in 2022.fwafwafawfef
                     Mainly focusing on web development, I have good understanding and can work both front end and
                     back-end roles</p>
-
-                <div className='flex gap-5'>
-                    <Link href="https://github.com/hnguyen96">
-                        <Image
-                            src="/icons/github.svg"
-                            width={25}
-                            height={25}
-                            alt="Github logo"
-                        />
-                    </Link>
-
-                    <Link href="mailto:hoangnamngx882@gmail.com">
-                        <Image
-                            src="/icons/mail.svg"
-                            width={25}
-                            height={25}
-                            alt="Mail logo"
-                        />
-                    </Link>
-                </div>
-
             </div>
-            <Image
-                    src="/nam.png"
-                    width={400}
-                    height={400}
-                    alt=""
-                />
+
+            <Image className='max-w-full h-auto'
+                src="/nam.png"
+                width={400}
+                height={400}
+                alt=""
+            />
         </div>
     )
 }
