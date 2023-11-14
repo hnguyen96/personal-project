af5d5a1c20edc0f14084bafba2fe397814636834
Nam <hoangnamngx882@gmail.com>
2023-11-14 15:08:44 -0800

    Signup responsive mobile changed

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   const router = useRouter();
 
   return (
-    <div className="font-serif md:flex flex-col md:min-h-screen items-center justify-center bg-home bg-cover">
+    <div className="font-serif flex flex-col min-h-screen items-center justify-center bg-home bg-cover">
       <div className="w-full md:w-[500px] p-6 bg-secondary md:rounded-lg">
         <div className="flex justify-center text-lg mx-auto breadcrumbs font-extrabold mb-5">
           <ul className="text-[#ce9c4b]">
