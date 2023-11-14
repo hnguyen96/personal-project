48d846990614fa771710c4234e5d9c16ce7a322f
Nam <hoangnamngx882@gmail.com>
2023-11-14 15:25:14 -0800

    Signup form padding fixed

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
 
   return (
     <div className="font-serif flex flex-col min-h-screen items-center justify-center bg-home bg-cover">
-      <div className="w-full md:w-[500px] p-10 bg-secondary md:rounded-lg">
+      <div className="w-full md:w-[500px] p-6 md:p-10 bg-secondary md:rounded-lg">
         <div className="flex justify-center text-lg mx-auto breadcrumbs font-extrabold mb-5">
           <ul className="text-[#ce9c4b]">
             <li><a onClick={() => router.push("/")}>Home</a></li>
